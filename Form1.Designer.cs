e236d0586462289234d3525c812e661877b0738c
messy <messy@SCOTT-DESKTOP>
2021-07-04 20:42:23 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace RemMKVtoMP4
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.mkvFileButton = new System.Windows.Forms.Button();
+			this.mp4FileButton = new System.Windows.Forms.Button();
+			this.convertButton = new System.Windows.Forms.Button();
+			this.mkvFileLabel = new System.Windows.Forms.Label();
+			this.mp4FileLabel = new System.Windows.Forms.Label();
+			this.conversionProgressLabel = new System.Windows.Forms.Label();
+			this.conversionProgressBar = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// mkvFileButton
+			// 
+			this.mkvFileButton.Location = new System.Drawing.Point(23, 22);
+			this.mkvFileButton.Name = "mkvFileButton";
+			this.mkvFileButton.Size = new System.Drawing.Size(115, 45);
+			this.mkvFileButton.TabIndex = 0;
+			this.mkvFileButton.Text = "MKV File";
+			this.mkvFileButton.UseVisualStyleBackColor = true;
+			this.mkvFileButton.Click += new System.EventHandler(this.mkvFileButton_Click);
+			// 
+			// mp4FileButton
+			// 
+			this.mp4FileButton.Location = new System.Drawing.Point(23, 93);
+			this.mp4FileButton.Name = "mp4FileButton";
+			this.mp4FileButton.Size = new System.Drawing.Size(115, 45);
+			this.mp4FileButton.TabIndex = 1;
+			this.mp4FileButton.Text = "MP4 File";
+			this.mp4FileButton.UseVisualStyleBackColor = true;
+			this.mp4FileButton.Click += new System.EventHandler(this.mp4FileButton_Click);
+			// 
+			// convertButton
+			// 
+			this.convertButton.Location = new System.Drawing.Point(23, 160);
+			this.convertButton.Name = "convertButton";
+			this.convertButton.Size = new System.Drawing.Size(115, 45);
+			this.convertButton.TabIndex = 2;
+			this.convertButton.Text = "Convert";
+			this.convertButton.UseVisualStyleBackColor = true;
+			this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+			// 
+			// mkvFileLabel
+			// 
+			this.mkvFileLabel.AutoSize = true;
+			this.mkvFileLabel.Location = new System.Drawing.Point(171, 37);
+			this.mkvFileLabel.Name = "mkvFileLabel";
+			this.mkvFileLabel.Size = new System.Drawing.Size(53, 15);
+			this.mkvFileLabel.TabIndex = 4;
+			this.mkvFileLabel.Text = "MKV File";
+			// 
+			// mp4FileLabel
+			// 
+			this.mp4FileLabel.AutoSize = true;
+			this.mp4FileLabel.Location = new System.Drawing.Point(171, 106);
+			this.mp4FileLabel.Name = "mp4FileLabel";
+			this.mp4FileLabel.Size = new System.Drawing.Size(52, 15);
+			this.mp4FileLabel.TabIndex = 5;
+			this.mp4FileLabel.Text = "MP4 File";
+			// 
+			// conversionProgressLabel
+			// 
+			this.conversionProgressLabel.AutoSize = true;
+			this.conversionProgressLabel.Location = new System.Drawing.Point(159, 175);
+			this.conversionProgressLabel.Name = "conversionProgressLabel";
+			this.conversionProgressLabel.Size = new System.Drawing.Size(119, 15);
+			this.conversionProgressLabel.TabIndex = 6;
+			this.conversionProgressLabel.Text = "Click Convert to Start";
+			// 
+			// conversionProgressBar
+			// 
+			this.conversionProgressBar.Location = new System.Drawing.Point(284, 171);
+			this.conversionProgressBar.Name = "conversionProgressBar";
+			this.conversionProgressBar.Size = new System.Drawing.Size(100, 23);
+			this.conversionProgressBar.TabIndex = 7;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(407, 217);
+			this.Controls.Add(this.conversionProgressBar);
+			this.Controls.Add(this.conversionProgressLabel);
+			this.Controls.Add(this.mp4FileLabel);
+			this.Controls.Add(this.mkvFileLabel);
+			this.Controls.Add(this.convertButton);
+			this.Controls.Add(this.mp4FileButton);
+			this.Controls.Add(this.mkvFileButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Rem\'s MKV to MP4 Converter";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button mkvFileButton;
+		private System.Windows.Forms.Button mp4FileButton;
+		private System.Windows.Forms.Button convertButton;
+		private System.Windows.Forms.Label mkvFileLabel;
+		private System.Windows.Forms.Label mp4FileLabel;
+		private System.Windows.Forms.Label conversionProgressLabel;
+		private System.Windows.Forms.ProgressBar conversionProgressBar;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FFmpeg.NET;
+using FFmpeg.NET.Events;
+
+namespace RemMKVtoMP4
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void mkvFileButton_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openMKVFileDialog = new OpenFileDialog
+			{
+				InitialDirectory = @"%USERPROFILE%\Videos",
+				Title = "Select MKV File",
+
+				CheckFileExists = true,
+				CheckPathExists = true,
+
+				DefaultExt = "mkv",
+				Filter = "mkv files (*.mkv)|*.mkv",
+				FilterIndex = 2,
+				RestoreDirectory = true
+			};
+
+			if (openMKVFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				mkvFileLabel.Text = openMKVFileDialog.FileName;
+			}
+		}
+
+		private void mp4FileButton_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog saveMP4FileDialog = new SaveFileDialog
+			{
+				InitialDirectory = @"%USERPROFILE%\Videos",
+				Title = "Save MP4 File",
+
+				CheckFileExists = false,
+				CheckPathExists = true,
+
+				DefaultExt = "mp4",
+				Filter = "mp4 files (*.mp4)|*.mp4",
+				FilterIndex = 2,
+				RestoreDirectory = true
+			};
+
+			if (saveMP4FileDialog.ShowDialog() == DialogResult.OK)
+			{
+				mp4FileLabel.Text = saveMP4FileDialog.FileName;
+			}
+		}
+
+		private async void convertButton_Click(object sender, EventArgs e)
+		{
+			await StartConversion();
+		}
+
+		public async Task StartConversion()
+		{
+			var ffmpeg = new Engine("C:\\FFMPEG\\bin\\ffmpeg.exe");
+			var inputMKV = new InputFile(@mkvFileLabel.Text.ToString());
+			var outputMP4 = new OutputFile(@mp4FileLabel.Text.ToString());
+
+			ffmpeg.Progress += OnProgress;
+			ffmpeg.Data += OnData;
+			ffmpeg.Error += OnError;
+			ffmpeg.Complete += OnComplete;
+
+			conversionProgressBar.Style = ProgressBarStyle.Marquee;
+			conversionProgressBar.MarqueeAnimationSpeed = 30;
+			await ffmpeg.ConvertAsync(inputMKV, outputMP4);
+			conversionProgressBar.MarqueeAnimationSpeed = 0;
+			conversionProgressBar.Style = ProgressBarStyle.Blocks;
+			conversionProgressBar.Value = 0;
+			string doneMessage = "Done";
+			MessageBox.Show(doneMessage);
+		}
+
+		private void OnProgress(object sender, ConversionProgressEventArgs e)
+		{
+			Debug.WriteLine("[{0} => {1}]", e.Input.Name, e.Output.Name);
+			Debug.WriteLine("Bitrate: {0}", e.Bitrate);
+			Debug.WriteLine("Fps: {0}", e.Fps);
+			Debug.WriteLine("Frame: {0}", e.Frame);
+			Debug.WriteLine("ProcessedDuration: {0}", e.ProcessedDuration);
+			Debug.WriteLine("Size: {0} kb", e.SizeKb);
+			Debug.WriteLine("TotalDuration: {0}\n", e.TotalDuration);
+
+		}
+
+		private void OnData(object sender, ConversionDataEventArgs e)
+		{
+			Debug.WriteLine("[{0} => {1}]: {2}", e.Input.Name, e.Output.Name, e.Data);
+		}
+
+		private void OnComplete(object sender, ConversionCompleteEventArgs e)
+		{
+			Debug.WriteLine("Completed conversion from {0} to {1}", e.Input.Name, e.Output.Name);
+		}
+
+		private void OnError(object sender, ConversionErrorEventArgs e)
+		{
+			Debug.WriteLine("[{0} => {1}]: Error: {2}\n{3}", e.Input.Name, e.Output.Name, e.Exception.ExitCode, e.Exception.InnerException);
+		}
+	}
+}
